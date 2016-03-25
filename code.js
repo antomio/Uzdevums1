f52520c0f0cb3fab5398219a4ef2d7ee38bd9ca0
antomio <toms.pirktins@gmail.com>
2016-03-25 19:48:56 +0200

    Algoritms++

--- code.js
+++ code.js
@@ -3,27 +3,69 @@
 
 	$("#button").on("click", function(e) {
 		e.preventDefault();
-		myValue($( "input[type='text']" ).val());
+		myValue($( "#red" ).val());
+		myValue2($( "#black" ).val());
+
+	$("#black").on("keyup", function() {
+		myValue2($( "#black" ).val());
+
+	});
   });
-	
+
 })(jQuery);
 
 	function myValue(s) {
 
 		if (isNaN(s) || s < 1) {
 			alert("Tabulā ir 0 elementu!");
+			
 		}
+
 		else {
+
 			for(var total = 1; s>0; s--){
 				total=total*s;
 			}
 
-		console.log(total);	
+		// console.log(total);
+		// console.log($("#black").val());
 
-			return total;
+		
+
+		$("#alert").text(total);
+
+		return total;
+
 		}
 
 	};
 
 	
+	function myValue2(a) {
+		console.log(a);
+		if (isNaN(a) || a < 1) {
+			alert("Tabulā ir 0 elementu!");
+			
+		}
+
+		else {
+
+			for(var total = 1; a>0; a--){
+				total=total*a;
+			}
+
+		// console.log(total);
+
+		// console.log($("#black").val());
+
+		
+
+		$("#alert2").text(total);
+
+		return total;
+
+		}
+
+	};
+
 
